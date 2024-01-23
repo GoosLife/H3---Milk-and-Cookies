abedd2c57b5ce5de26ab651c1820521912b4b967
Frederik H. Sørensen <72657308+GoosLife@users.noreply.github.com>
2024-01-23 13:15:18 +0100

    Add project files.

---
+++ H3 - Milk and Cookies/Controllers/MilkshakeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace H3___Milk_and_Cookies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MilkshakeController : Controller
+    {
+        [HttpGet]
+        public string Get(string favoriteMilkshake)
+        {
+            Response.Cookies.Append("favoriteMilkshake", favoriteMilkshake);
+            return favoriteMilkshake;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public string GetFromCookie()
+        {
+            if (Request.Cookies["favoriteMilkshake"] == null)
+            {
+                Response.StatusCode = 404;
+                return "Ya haven't set a favorite milkshake ya oaf";
+            }
+
+            return Request.Cookies["favoriteMilkshake"];
+        }
+    }
+}
